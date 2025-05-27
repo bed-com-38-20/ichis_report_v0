7def1b2df3407ddc27709190cb12ba6ede47dba3
bed-com-38-20 <bed-com-38-20@unima.ac.mw>
2025-05-27 20:05:55 +0200

    customizing cells

--- src/pages/tables/edit-table-template/EditTableCell/ContentTypeSelector.js
+++ src/pages/tables/edit-table-template/EditTableCell/ContentTypeSelector.js
@@ -1,34 +1,42 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { SingleSelectField, SingleSelectOption } from '@dhis2/ui'
-import i18n from '../../../../locales'
-import contentTypes from '../../../../modules/contentTypes'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SingleSelectField, SingleSelectOption } from '@dhis2/ui';
+import i18n from '../../../../locales';
+import contentTypes from '../../../../modules/contentTypes';
+import classes from './ContentTypeSelector.module.css';
+import Icon from '../../../../components/Icon';
 
 export function ContentTypeSelector({ currentContentType, onChange }) {
     return (
-        <div className="container">
+        <div className={classes.container}>
+            <div className={classes.labelWrapper}>
+                <Icon name="list" className={classes.icon} />
+                <label className={classes.label}>
+                    {i18n.t('Content type', { defaultValue: 'Content type' })}
+                </label>
+            </div>
             <SingleSelectField
-                label={i18n.t('Content type')}
+                className={classes.select}
                 onChange={({ selected }) => onChange(selected)}
                 selected={currentContentType}
                 dense
             >
                 {Object.values(contentTypes).map(({ id, getName }) => (
-                    <SingleSelectOption value={id} label={getName()} key={id} />
+                    <SingleSelectOption
+                        value={id}
+                        label={getName()}
+                        key={id}
+                        className={classes.option}
+                    />
                 ))}
             </SingleSelectField>
-            <style jsx>{`
-                .container {
-                    padding: 0 8px;
-                }
-            `}</style>
         </div>
-    )
+    );
 }
 
 ContentTypeSelector.propTypes = {
     onChange: PropTypes.func.isRequired,
     currentContentType: PropTypes.string,
-}
+};
 
-export default ContentTypeSelector
+export default ContentTypeSelector;
